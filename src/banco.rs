e9d5dc9e1af0a00916e93fbb37ecc49f8aab8570
thesomeypo <someypo@gmail.com>
2026-05-10 11:50:38 -0300

    database update, timer increment

--- src/banco.rs
+++ src/banco.rs
@@ -1,4 +1,4 @@
-use rusqlite::{params, Connection, Result};
+use rusqlite::{Connection, Result, params};
 
 pub fn conectar() -> Result<Connection> {
     Connection::open("plantcoin.db")
@@ -11,12 +11,23 @@ pub fn iniciar_banco() -> Result<()> {
             id INTEGER PRIMARY KEY,
             telegram_id INTEGER UNIQUE,
             saldo REAL DEFAULT 0.0,
-            conheita_cooldown INTEGER DEFAULT 0
+            colheita_cooldown INTEGER DEFAULT 0
         )",
         [],
     )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS codigos_pendentes (
+        codigo TEXT PRIMARY KEY,
+        telegram_id INTEGER,
+        criado_em INTEGER
+        )",
+        []
+    )?;
+    
     Ok(())
 }
+
 pub fn ver_saldo(telegram_id: i64) -> Result<f64> {
     let conn = conectar()?;
 
@@ -45,5 +56,55 @@ pub fn adicionar_saldo(telegram_id: i64, valor: f64) -> Result<()> {
         "UPDATE usuarios SET saldo = saldo + ?1 WHERE telegram_id = ?2",
         params![valor, telegram_id],
     )?; 
+    Ok(())
+}
+
+pub fn cooldown (telegram_id: i64) -> Result<i64> {
+    let conn = conectar()?;
+
+    let mut ultima_vez = conn.prepare("SELECT colheita_cooldown FROM usuarios WHERE telegram_id = ?1")?;
+    let mut rows = ultima_vez.query([telegram_id])?;
+
+    if let Some(row) = rows.next()? {
+        let cooldown: i64 = row.get(0)?;
+        Ok(cooldown)
+    } else {
+        conn.execute("INSERT INTO usuarios (telegram_id, colheita_cooldown) VALUES (?1, 0)",
+            [telegram_id]
+        )?;
+        Ok(0)
+    }
+}
+
+pub fn atualizar_cooldown (telegram_id: i64, novo_timer: i64) -> Result<()> {
+    let conn = conectar()?;
+    let _ = cooldown(telegram_id);
+
+    conn.execute(
+        "UPDATE usuarios SET colheita_cooldown = ?1 WHERE telegram_id = ?2",
+        params![novo_timer, telegram_id],
+    )?;
+    Ok(())
+}
+
+pub fn salvar_codigo (telegram_id: i64, codigo: &str, agora: i64) -> Result<()> {
+    let conn = conectar()?;
+    
+    conn.execute(
+        "INSERT INTO codigos_pendentes (codigo, telegram_id, criado_em) VALUES(?1, ?2, ?3)",
+        params![codigo, telegram_id , agora],
+    )?;
+
+    Ok(())
+}
+
+pub fn limpar_codigo (codigo: &str) -> Result<()> {
+    let conn = conectar()?;
+
+    conn.execute(
+        "DELETE FROM codigos_pendentes WHERE codigo = ?1",
+        [codigo],
+    )?;
+
     Ok(())
 }
--- src/telegram.rs
+++ src/telegram.rs
@@ -1,101 +1,155 @@
-use teloxide::{prelude::*, types::{InlineKeyboardButton, InlineKeyboardMarkup}, utils::command::BotCommands
+use teloxide::{
+    prelude::*,
+    types::{InlineKeyboardButton, InlineKeyboardMarkup},
+    utils::command::BotCommands,
 };
 
-use crate::telegram;
+use chrono::Utc;
 
 #[derive(BotCommands, Clone)]
 #[command(rename_rule = "lowercase")]
 
 pub enum Command {
-    Start,
+    Start(String),
     Username,
     Saldo,
     Colher,
 }
 
 pub async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    let user_id = msg.from.as_ref().unwrap().id.0;
+    let chat_id = msg.chat.id;
+
     match cmd {
-        
-        Command::Start => {
-            let username = msg.from
+        Command::Start(codigo) => {
+            let username = msg
+                .from
                 .as_ref()
                 .and_then(|user| user.username.clone())
                 .unwrap_or_else(|| String::from("membro"));
 
-            let texto = format!("Oi tudo bem @{}", username);
+            let keyboard = InlineKeyboardMarkup::new(vec![vec![
+                InlineKeyboardButton::callback("Ver Username", "btn_username"),
+                InlineKeyboardButton::callback("📊 Saldo", "btn_saldo"),
+                InlineKeyboardButton::callback("💰 Colher", "btn_colher"),
+            ]]);
 
-            let keyboard = InlineKeyboardMarkup::new(vec![
-                vec![
-                    InlineKeyboardButton::callback("Ver Username", "btn_username"),
-                    InlineKeyboardButton::callback("📊 Saldo", "btn_saldo"),
-                    InlineKeyboardButton::callback("💰 Colher", "btn_colher"),
-                ]
-            ]);
+            let codigo_premio = "dinheiro";
 
-            bot.send_message(msg.chat.id, texto)
-                .reply_markup(keyboard)
-                .await?;
+            if codigo == codigo_premio {
+                let texto = format!(
+                    "Voce usou o codigo {} acabou de desbloquear a recompensa",
+                    codigo_premio
+                );
+                bot.send_message(chat_id, texto).await?;
+            } else {
+                let texto = format!("Oi tudo bem @{}", username);
+                bot.send_message(chat_id, texto)
+                    .reply_markup(keyboard)
+                    .await?;
+            }
         }
 
         Command::Username => {
-            let username = msg.from
+            let username = msg
+                .from
                 .as_ref()
                 .and_then(|user| user.username.clone())
                 .unwrap_or_else(|| String::from("Desconhecido"));
 
             let texto = format!("Seu nome de usuario é @{}", username);
 
-            bot.send_message(msg.chat.id, texto).await?;
+            bot.send_message(chat_id, texto).await?;
         }
 
         Command::Saldo => {
-            let saldo = crate::banco::ver_saldo(msg.chat.id.0).unwrap_or(0.0);
+            let saldo = crate::banco::ver_saldo(user_id as i64).unwrap_or(0.0);
             let texto = format!("Seu Saldo atual é de R${}", saldo);
 
-            bot.send_message(msg.chat.id, texto).await?;
+            bot.send_message(chat_id, texto).await?;
         }
 
         Command::Colher => {
             let lucro_shortlink_1: f64 = 0.005;
-            let add_saldo = crate::banco::adicionar_saldo(msg.chat.id.0, lucro_shortlink_1);
-            let texto = format!("Parabens voce ganhou R${}", lucro_shortlink_1);
+            let hora_em_segundos: i64 = 3600;
+            let agora = Utc::now().timestamp();
+            let ultima_coleta: i64 = crate::banco::cooldown(user_id as i64).unwrap_or(0);
+            let calculo_se_pode = agora - ultima_coleta;
 
-            bot.send_message(msg.chat.id, texto).await?;
-            
+            if calculo_se_pode >= hora_em_segundos {
+                let _ = crate::banco::adicionar_saldo(user_id as i64, lucro_shortlink_1);
+                let texto = format!("Parabens voce ganhou R${}", lucro_shortlink_1);
+                let _ = crate::banco::atualizar_cooldown(user_id as i64, agora);
+
+                bot.send_message(chat_id, texto).await?;
+            } else {
+                let faltam_tempo = (hora_em_segundos - calculo_se_pode) / 60;
+                let texto = format!(
+                    "Não é possivel coletar ainda faltam {} Minutos",
+                    faltam_tempo
+                );
+
+                bot.send_message(chat_id, texto).await?;
+            }
         }
     }
     Ok(())
 }
+
 pub async fn cb_handler(bot: Bot, q: CallbackQuery) -> ResponseResult<()> {
     if let Some(data) = q.data.clone() {
+        let user_id = q.from.id.0;
+        let chat_id = q.message.as_ref().map(|m| m.chat().id);
+
         match data.as_str() {
             "btn_username" => {
-                let username = q.from.username.clone().unwrap_or_else(|| String::from("Desconhecido"));
+                let username = q
+                    .from
+                    .username
+                    .clone()
+                    .unwrap_or_else(|| String::from("Desconhecido"));
                 let texto = format!("Seu nome de usuario é @{}", username);
 
-                if let Some(msg) = q.regular_message() {
-                    bot.send_message(msg.chat.id, texto).await?;
+                if let Some(c_id) = chat_id {
+                    bot.send_message(c_id, texto).await?;
                 }
             }
 
             "btn_saldo" => {
-                if let Some(msg) = q.regular_message() {
-                    let saldo = crate::banco::ver_saldo(msg.chat.id.0).unwrap_or(0.0);
-                    let texto = format!("Seu Saldo atual é de R${}", saldo);
-                    bot.send_message(msg.chat.id, texto).await?;
+                let saldo = crate::banco::ver_saldo(user_id as i64).unwrap_or(0.0);
+                let texto = format!("Seu Saldo atual é de R${}", saldo);
+
+                if let Some(c_id) = chat_id {
+                    bot.send_message(c_id, texto).await?;
                 }
             }
 
             "btn_colher" => {
                 let lucro_shortlink_1: f64 = 0.005;
-                let texto = format!("Parabens voce ganhou R${}!", lucro_shortlink_1);
+                let agora = Utc::now().timestamp();
+                let hora_em_segundo: i64 = 3600;
+                let ultima_coleta: i64 = crate::banco::cooldown(user_id as i64).unwrap_or(0);
+                let calculo_se_pode = agora - ultima_coleta;
 
-                if let Some(msg) = q.regular_message() {
-                    let add_saldo = crate::banco::adicionar_saldo(msg.chat.id.0, lucro_shortlink_1);
-                    bot.send_message(msg.chat.id, texto).await?;
+                if let Some(c_id) = chat_id {
+                    if calculo_se_pode >= hora_em_segundo {
+                        let _ = crate::banco::adicionar_saldo(user_id as i64, lucro_shortlink_1);
+                        let texto = format!("Parabens voce ganhou R${}!", lucro_shortlink_1);
+                        let _ = crate::banco::atualizar_cooldown(user_id as i64, agora);
+
+                        bot.send_message(c_id, texto).await?;
+                    } else {
+                        let faltam_tempo = (hora_em_segundo - calculo_se_pode) / 60;
+                        let texto = format!(
+                            "Não é possivel coletar ainda faltam {} Minutos",
+                            faltam_tempo
+                        );
+
+                        bot.send_message(c_id, texto).await?;
+                    }
                 }
             }
-            
+
             _ => {}
         }
     }
